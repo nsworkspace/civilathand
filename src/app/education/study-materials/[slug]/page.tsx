d26c65bc9cb8e8c337419ba681da4c8774507605
NS WorkSpace <nikhilsain.workspace@gmail.com>
2026-09-26 17:27:33 +0530

    Delete src/app/education directory

--- src/app/education/study-materials/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Education section removed from the website: send visitors to the home page.
-export default function RemovedPage() {
-  redirect("/");
-}
